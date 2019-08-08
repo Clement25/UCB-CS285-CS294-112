cc3b163f94b44b8925f1b812cfbfbda4e9416b66
unknown <935974082@qq.com>
2019-08-08 12:31:06 +0800

    modify tested model

--- hw3/run2.4.1.sh
+++ hw3/run2.4.1.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-python train_ac_f18.py CartPole-v0 -n 100 -b 1000 -e 3 --exp_name 1_1 -ntu 1 -ngsptu 1 
+python train_ac_f18.py CartPole-v0 -n 100 -b 1000 -e 3 --exp_name 1_1 -ntu 1 -ngsptu 1
 
 python train_ac_f18.py CartPole-v0 -n 100 -b 1000 -e 3 --exp_name 100_1 -ntu 100 -ngsptu 1
 python train_ac_f18.py CartPole-v0 -n 100 -b 1000 -e 3 --exp_name 1_100 -ntu 1 -ngsptu 100
--- hw3/run2.4.2.sh
+++ hw3/run2.4.2.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
-python train_ac_f18.py InvertedPendulum-v2 -ep 1000 --discount 0.95 -n 100 -e 3 -l 2 -s 64 -b 5000 -lr 0.01 \
+python train_ac_f18.py InvertedPendulum-v1 -ep 1000 --discount 0.95 -n 100 -e 3 -l 2 -s 64 -b 5000 -lr 0.01 \
 --exp_name 10_10 -ntu 10 -ngsptu 10
 
-python train_ac_f18.py HalfCheetah-v2 -ep 150 --discount 0.90 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 \
+python train_ac_f18.py HalfCheetah-v1 -ep 150 --discount 0.90 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 \
 --exp_name 10_10 -ntu 10 -ngsptu 10
